2bed4233d13b9c7dccf6993a326f90104b65c8a2
ThomasRochefortB <thomas.rochefort.beaudoin@gmail.com>
2024-01-31 13:24:43 -0500

    m

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -41,12 +41,72 @@ class YOLODataset(BaseDataset):
         assert not (self.use_segments and self.use_keypoints), "Can not use both segments and keypoints."
         super().__init__(*args, **kwargs)
 
+    # def cache_labels(self, path=Path("./labels.cache")):
+    #     """
+    #     Cache dataset labels, check images and read shapes.
+
+    #     Args:
+    #         path (Path): path where to save the cache file (default: Path('./labels.cache')).
+    #     Returns:
+    #         (dict): labels.
+    #     """
+    #     x = {"labels": []}
+    #     nm, nf, ne, nc, msgs = 0, 0, 0, 0, []  # number missing, found, empty, corrupt, messages
+    #     desc = f"{self.prefix}Scanning {path.parent / path.stem}..."
+    #     total = len(self.im_files)
+    #     nkpt, ndim = self.data.get("kpt_shape", (0, 0))
+    #     if self.use_keypoints and (nkpt <= 0 or ndim not in (2, 3)):
+    #         raise ValueError("'kpt_shape' in data.yaml missing or incorrect. Should be a list with [number of "
+    #                         "keypoints, number of dims (2 for x,y or 3 for x,y,visible)], i.e. 'kpt_shape: [17, 3]'")
+        
+    #     with ThreadPool(NUM_THREADS) as pool:
+    #         results = pool.imap(func=verify_image_label,
+    #                             iterable=zip(self.im_files, self.label_files, repeat(self.prefix),
+    #                                         repeat(self.use_keypoints), repeat(len(self.data['names'])), repeat(nkpt),
+    #                                         repeat(ndim)))
+    #         pbar = TQDM(results, desc=desc, total=total)
+    #         # Change the unpacking here to include regression_vars
+    #         for im_file, lb, shape, segments, regression_vars, keypoint, nm_f, nf_f, ne_f, nc_f, msg in pbar:
+    #             nm += nm_f
+    #             nf += nf_f
+    #             ne += ne_f
+    #             nc += nc_f
+    #             if im_file:
+    #                 x["labels"].append(
+    #                     dict(
+    #                         im_file=im_file,
+    #                         shape=shape,
+    #                         cls=lb[:, 0:1],  # n, 1
+    #                         bboxes=lb[:, 1:],  # n, 4
+    #                         regression_vars=regression_vars,  # n, 6
+    #                         segments=segments,
+    #                         keypoints=keypoint,
+    #                         normalized=True,
+    #                         bbox_format="xywh",
+    #                     )
+    #                 )
+                
+    #             if msg:
+    #                 msgs.append(msg)
+    #             pbar.desc = f"{desc} {nf} images, {nm + ne} backgrounds, {nc} corrupt"
+    #         pbar.close()
+
+    #     if msgs:
+    #         LOGGER.info("\n".join(msgs))
+    #     if nf == 0:
+    #         LOGGER.warning(f"{self.prefix}WARNING ⚠️ No labels found in {path}. {HELP_URL}")
+    #     x["hash"] = get_hash(self.label_files + self.im_files)
+    #     x["results"] = nf, nm, ne, nc, len(self.im_files)
+    #     x["msgs"] = msgs  # warnings
+    #     save_dataset_cache_file(self.prefix, path, x)
+    #     return x
     def cache_labels(self, path=Path("./labels.cache")):
         """
         Cache dataset labels, check images and read shapes.
 
         Args:
-            path (Path): path where to save the cache file (default: Path('./labels.cache')).
+            path (Path): Path where to save the cache file. Default is Path('./labels.cache').
+
         Returns:
             (dict): labels.
         """
@@ -56,17 +116,25 @@ class YOLODataset(BaseDataset):
         total = len(self.im_files)
         nkpt, ndim = self.data.get("kpt_shape", (0, 0))
         if self.use_keypoints and (nkpt <= 0 or ndim not in (2, 3)):
-            raise ValueError("'kpt_shape' in data.yaml missing or incorrect. Should be a list with [number of "
-                            "keypoints, number of dims (2 for x,y or 3 for x,y,visible)], i.e. 'kpt_shape: [17, 3]'")
-        
+            raise ValueError(
+                "'kpt_shape' in data.yaml missing or incorrect. Should be a list with [number of "
+                "keypoints, number of dims (2 for x,y or 3 for x,y,visible)], i.e. 'kpt_shape: [17, 3]'"
+            )
         with ThreadPool(NUM_THREADS) as pool:
-            results = pool.imap(func=verify_image_label,
-                                iterable=zip(self.im_files, self.label_files, repeat(self.prefix),
-                                            repeat(self.use_keypoints), repeat(len(self.data['names'])), repeat(nkpt),
-                                            repeat(ndim)))
+            results = pool.imap(
+                func=verify_image_label,
+                iterable=zip(
+                    self.im_files,
+                    self.label_files,
+                    repeat(self.prefix),
+                    repeat(self.use_keypoints),
+                    repeat(len(self.data["names"])),
+                    repeat(nkpt),
+                    repeat(ndim),
+                ),
+            )
             pbar = TQDM(results, desc=desc, total=total)
-            # Change the unpacking here to include regression_vars
-            for im_file, lb, shape, segments, regression_vars, keypoint, nm_f, nf_f, ne_f, nc_f, msg in pbar:
+            for im_file, lb, shape, segments, keypoint, nm_f, nf_f, ne_f, nc_f, msg in pbar:
                 nm += nm_f
                 nf += nf_f
                 ne += ne_f
@@ -78,14 +146,12 @@ class YOLODataset(BaseDataset):
                             shape=shape,
                             cls=lb[:, 0:1],  # n, 1
                             bboxes=lb[:, 1:],  # n, 4
-                            regression_vars=regression_vars,  # n, 6
                             segments=segments,
                             keypoints=keypoint,
                             normalized=True,
                             bbox_format="xywh",
                         )
                     )
-                
                 if msg:
                     msgs.append(msg)
                 pbar.desc = f"{desc} {nf} images, {nm + ne} backgrounds, {nc} corrupt"
